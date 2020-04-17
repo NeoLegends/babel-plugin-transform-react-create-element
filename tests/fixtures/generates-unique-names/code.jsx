4c32b406706a01e9f3b66397d14cc5bebf1162b0
Moritz Gunz <moritz.gunz@gmail.com>
2020-04-17 14:47:08 +0200

    :white_check_mark: Verify local variable name is unique

---
+++ tests/fixtures/generates-unique-names/code.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const _createElement = 1 + 2;
+const element = <div>Hello, World!</div>;
---
+++ tests/fixtures/generates-unique-names/output.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const _createElement2 = /*#__PURE__*/ React.createElement;
+
+const _createElement = 1 + 2;
+
+const element = /*#__PURE__*/ _createElement2("div", null, "Hello, World!");
